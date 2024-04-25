b372955de9fc0fa6eb01396d613b03aa841c16cb
Yoga Raditya <radityanalaa@gmail.com>
2024-04-25 12:57:48 +0700

    chore: Add Pelanggan

---
+++ src/Pelanggan.java
@@ -0,0 +1,45 @@
+public class Pelanggan {
+    private int kodePelanggan;
+    private String nama;
+    // currently using String, use Enum if possible
+    private String statusKeanggotaan;
+    private Lagu[] playlist;
+
+    public Pelanggan(int kodePelanggan, String nama, String statusKeanggotaan) {
+        this.kodePelanggan = kodePelanggan;
+        this.nama = nama;
+        this.statusKeanggotaan = statusKeanggotaan;
+    }
+
+    public int getKodePelanggan() {
+        return kodePelanggan;
+    }
+
+    public void setKodePelanggan(int kodePelanggan) {
+        this.kodePelanggan = kodePelanggan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getStatusKeanggotaan() {
+        return statusKeanggotaan;
+    }
+
+    public void setStatusKeanggotaan(String statusKeanggotaan) {
+        this.statusKeanggotaan = statusKeanggotaan;
+    }
+
+    public Lagu[] getPlaylist() {
+        return playlist;
+    }
+
+    public void setPlaylist(Lagu[] playlist) {
+        this.playlist = playlist;
+    }
+}
